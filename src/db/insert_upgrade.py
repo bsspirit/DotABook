add1f398e5dce0eb406ab88e39557891c2cf9f90
bsspirit <bsspirit@163.com>
2011-03-28 12:52:10 +0800

    update

--- src/db/insert_upgrade.py
+++ src/db/insert_upgrade.py
@@ -6,6 +6,8 @@ def insert(db):
 	db.session.add(Upgrade(20110324,'在首页增加新浪登录提示','在首页增加新浪登录提示'))
 	db.session.add(Upgrade(20110324,'增加英雄评分发布到微博提示','增加英雄评分发布到微博提示'))
 	db.session.add(Upgrade(20110325,'增加每日更新提示','增加每日更新提示'))
+	db.session.add(Upgrade(20110325,'英雄详细信息界面更新','英雄详细信息界面更新'))
+	db.session.add(Upgrade(20110327,'增加英雄评论','增加英雄评论'))
 
 
 if __name__ == '__main__':
