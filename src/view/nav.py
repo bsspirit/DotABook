47dcb00cb5912cc2f16c81413806062a32f0c141
bsspirit <bsspirit@163.com>
2011-04-16 15:06:05 +0800

    index

--- src/view/nav.py
+++ src/view/nav.py
@@ -1,19 +1,27 @@
 # -*- coding: utf-8 -*-
-from flask import Module, render_template, session, redirect, url_for, current_app
+from flask import Module, render_template, session, redirect, url_for, current_app, request
 from db.create import Upgrade
+from db.create_sina import Sina_Tweet,Sina_User
 import util.mydate as mydate
 
 view = Module(__name__)
 
 @view.route('/home')
 def index():
-	if session.get('path',False):
-		redirect(url_for('init_session'))
-
-#	tweets = Sina_Tweet.query.order_by(Sina_Tweet.id.desc()).all()
-#	ts = [t.json() for t in tweets]
+	if not session.get('path',False):redirect(url_for('init'))
 	
-	return render_template('index.html')
+	p,count = int(request.args.get('p','1')),10
+	tweets_page = Sina_Tweet.query.order_by(Sina_Tweet.id.desc()).paginate(p,count)
+	page={'p':p,'count':count,'total':tweets_page.total}
+	
+	ts,users,rets = [],{},{}
+	for t in tweets_page.items:
+		users[t.uid] = Sina_User.query.filter(Sina_User.uid==t.uid).first()
+		ts.append(t.json())
+		if t.retid:rets[t.retid] = Sina_Tweet.query.filter(Sina_Tweet.tid==t.retid).first()
+		
+	
+	return render_template('index.html', tweets=ts, retweets=rets, users=users, page=page)
 
 @view.route('/logout')
 def logout():
@@ -26,15 +34,20 @@ def logout():
 
 @view.route('/contact')
 def contact():
+	if not session.get('path',False):redirect(url_for('init'))
+	
 	return render_template('contact.html')
 	
 @view.route('/about')
 def about():
+	if not session.get('path',False):redirect(url_for('init'))
+	
 	return render_template('about.html')
 	
 @view.route('/')
-def init_session():
-	if session.get('path',False):
+def init():
+	if not session.get('path',False):
+		current_app.logger.info('path false')
 		session['login']=session.get('login',False)
 		session['admin']=session.get('admin',False)
 	
--- src/view/tweet.py
+++ src/view/tweet.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import Module, current_app, session, jsonify
+from flask import Module, current_app, session, jsonify, request
 from db.create_sina import Sina_User,Sina_Tweet
 import util.mydate as mydate
 from sina.sinaAPI import sinaAPI
@@ -8,8 +8,19 @@ view = Module(__name__)
 
 @view.route('/tweets')
 def tweet():
-	tweets = Sina_Tweet.query.order_by(Sina_Tweet.id.desc()).all()
-	ts = [t.json() for t in tweets]
-	return jsonify(tweets=ts)
+	p,count = int(request.args.get('p','1')),10
+	
+	tweets_page = Sina_Tweet.query.order_by(Sina_Tweet.id.desc()).paginate(p,count)
+	page={'p':p,'count':count,'total':tweets_page.total}
+	
+	ts,users,rets = [],{},{}
+	for t in tweets_page.items:
+		users[t.uid] = Sina_User.query.filter(Sina_User.uid==t.uid).first().json()
+		ts.append(t.json())
+		
+		if t.retid:
+			rets[t.retid] = Sina_Tweet.query.filter(Sina_Tweet.tid==t.retid).first().json()
+	
+	return jsonify(tweets=ts, retweets=rets, users=users, page=page)
 
 
